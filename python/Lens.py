a783971997b5d8567efdcc3f178c98a985d1a7b2
Amarth Gul <amarthgul@gmail.com>
2024-09-06 19:03:52 -0400

    Added new classes

---
+++ python/Lens.py
@@ -0,0 +1,12 @@
+
+
+
+class Lens:
+    def __inti__(self):
+        self.elements = []
+        
+    def UpdateLens(self):
+        # Update the attributes 
+        pass 
+
+
---
+++ python/Surface.py
@@ -0,0 +1,15 @@
+
+
+
+
+
+class Surface:
+    def __init__(self):
+        self.radius = None
+        self.thickness = None
+        self.material = None
+        self.clearSemiDiameter = None 
+        self.chamfer = None 
+
+        # Add asph 
+        
