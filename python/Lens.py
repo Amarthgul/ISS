Added ext 3 formula and fixed on axis refraction

python/Lens.py
@@ -130,7 +130,7 @@ class Lens:
         return posC + vecPCN * t 
 
 
-    def _ellipsePeripheral(self, posA, posB, posC, posP, d, r, useDistribution = True):
+    def _ellipsePeripheral(self, posA, posB, posC, posP, sd, r, useDistribution = True):
         """
         Find the points on the ellipse and align it to the AB plane. 
 
@@ -138,60 +138,80 @@ class Lens:
         :param posB: position of point B. 
         :param posC: position of point C. 
         :param posP: position of point P. 
-        :param d: clear semi diameter of the surface. 
+        :param sd: clear semi diameter of the surface. 
         :param r: radius of the surface. 
         :param useDistribution: when enabled, the method returns a distribution of points in the ellipse area instead of the points representing the outline of the ellipse. 
         """
         offset = np.array([0, 0, posA[2]])
-
-        # Util vectors 
-        P_xy_projection = Normalized(np.array([posP[0], posP[1], 0]))
+        onAxis = False 
 
         # On axis scenario 
-        if (np.linalg.norm(P_xy_projection) == 0):
-            P_xy_projection = np.array([d, 0, 0])
+        if (np.isclose(posP[0], 0) and np.isclose(posP[1], 0)):
+            P_xy_projection = np.array([sd, 0, 0])
+            onAxis = True 
+        else:
+            # Util vectors 
+            P_xy_projection = Normalized(np.array([posP[0], posP[1], 0]))
 
         vecCA = posA - posC
         
-        # Lengths to calculate semi-major axis length 
-        BB = abs((2 * d) * ((posP[2] - posB[2]) / posP[2]))
-        AC = np.linalg.norm(posA - posC)
+        # On axis rays can be grealty simplified 
+        if(onAxis):
+            if (useDistribution):
+                # Move the point along the z axis 
+                points = np.transpose(CircularDistribution()) + offset
+                # Scale it on the two semi-major axis 
+                points = np.transpose(points * np.array([sd, sd, 1]))
+                
+            else:
+                # Generate the contour of the ellipse 
+                theta = np.linspace(0, 2 * np.pi, 100)
+                x = np.cos(theta) 
+                y = np.sin(theta) 
+                z = np.ones(len(x)) * posA[2]
+                points = np.array([x, y, z])
+            return points 
         
-        # Semi-major axis length 
-        a = np.linalg.norm(posA - posB) / 2
-        b = np.sqrt(BB * AC) / 2
-
-        # Calculate the ellipse 
-        if (useDistribution):
-            # Move the point along the z axis 
-            points = np.transpose(CircularDistribution()) + offset
-            # Scale it on the two semi-major axis 
-            points = np.transpose(points * np.array([b, a, 1]))
-            
         else:
-            # Generate the contour of the ellipse 
-            theta = np.linspace(0, 2 * np.pi, 100)
-            x = b * np.cos(theta) 
-            y = a * np.sin(theta) 
-            z = np.ones(len(x)) * posA[2]
-            points = np.array([x, y, z])
-        
-        # Rotate the ellipse to it faces the right direction in the world xy plane,
-        # i.e., one of its axis coincides with the tangential plane 
-        theta_1 = angleBetweenVectors(posA, np.array([0, 1, 0]))
-        trans_1 = Rotation(-theta_1, np.array([0, 0, 1]), points)
-        
-        # Move the points to be in tangent with A 
-        trans_1 = Translate(trans_1, P_xy_projection * (d - a)) 
-        
-        # Rotate the ellipse around A it fits into the AB plane 
-        theta = angleBetweenVectors(posB-posA, posC-posA)
-        axis = Normalized(np.array([-vecCA[1], vecCA[0], 0]))
-        trans_2 = Translate(trans_1, -posA)
-        trans_2 = Rotation(-theta, axis, trans_2)
-        trans_2 = Translate(trans_2, posA)
+            # Lengths to calculate semi-major axis length 
+            BB = abs((2 * sd) * ((posP[2] - posB[2]) / posP[2]))
+            AC = np.linalg.norm(posA - posC)
+            
+            # Semi-major axis length 
+            a = np.linalg.norm(posA - posB) / 2
+            b = np.sqrt(BB * AC) / 2
 
-        return trans_2
+            # Calculate the ellipse 
+            if (useDistribution):
+                # Move the point along the z axis 
+                points = np.transpose(CircularDistribution()) + offset
+                # Scale it on the two semi-major axis 
+                points = np.transpose(points * np.array([b, a, 1]))
+                
+            else:
+                # Generate the contour of the ellipse 
+                theta = np.linspace(0, 2 * np.pi, 100)
+                x = b * np.cos(theta) 
+                y = a * np.sin(theta) 
+                z = np.ones(len(x)) * posA[2]
+                points = np.array([x, y, z])
+            
+            # Rotate the ellipse to it faces the right direction in the world xy plane,
+            # i.e., one of its axis coincides with the tangential plane 
+            theta_1 = angleBetweenVectors(posA, np.array([0, 1, 0]))
+            trans_1 = Rotation(-theta_1, np.array([0, 0, 1]), points)
+            
+            # Move the points to be in tangent with A 
+            trans_1 = Translate(trans_1, P_xy_projection * (sd - a)) 
+            
+            # Rotate the ellipse around A it fits into the AB plane 
+            theta = angleBetweenVectors(posB-posA, posC-posA)
+            axis = Normalized(np.array([-vecCA[1], vecCA[0], 0]))
+            trans_2 = Translate(trans_1, -posA)
+            trans_2 = Rotation(-theta, axis, trans_2)
+            trans_2 = Translate(trans_2, posA)
+
+            return trans_2
 
 
     def _surfaceIntersection(self, surfaceIndex):
@@ -382,8 +402,12 @@ class Lens:
 
         P_xy_projection = np.array([posP[0], posP[1], 0])
         offset = np.array([0, 0, abs(r) - np.sqrt(r**2 - sd**2)]) * np.sign(r)
-        posA = sd * ( P_xy_projection / np.linalg.norm(P_xy_projection) ) + offset
-        posC = sd * (-P_xy_projection / np.linalg.norm(P_xy_projection) ) + offset
+        if(not np.isclose(np.linalg.norm(P_xy_projection), 0)):
+            posA = sd * ( P_xy_projection / np.linalg.norm(P_xy_projection) ) + offset
+            posC = sd * (-P_xy_projection / np.linalg.norm(P_xy_projection) ) + offset
+        else:
+            posA = np.array([sd, 0, 0]) + offset
+            posC = np.array([sd, 0, 0]) + offset
         posB = self._findB(posA, posC, posP)
 
         points = np.transpose(self._ellipsePeripheral(posA, posB, posC, posP, sd, r)) # Sample points in the ellipse area 

python/Material.py
@@ -54,7 +54,9 @@ class Material:
             elif (formula == "Sellmeier1"):
                 self.Formula = "Sellmeier1"
                 self._decodeSellmeier1(found)
-
+            elif (formula == "Extended 3"):
+                self.Formula = "Extended 3"
+                self._decodeExtended_3(found)
     
 
     # ============================================================================
@@ -65,6 +67,8 @@ class Material:
             return self._Schott(wavelength)
         elif(self.Formula == "Sellmeier1"):
             return self._Sellmeier1(wavelength)
+        elif(self.Formula == "Extended 3"):
+            return self._Extended_3(wavelength)
         
     def _decodeSchott(self, df):
         df = df.to_numpy()
@@ -114,11 +118,39 @@ class Material:
         n2 = (k1 * lam**2) / (lam**2 - l1) + (k2 * lam**2) / (lam**2 - l2) + (k3 * lam**2) / (lam**2 - l3) + 1
         return np.sqrt(n2) 
 
+    def _decodeExtended_3(self, df):
+        df = df.to_numpy()
+        self.coef = [
+            df[np.where(df=="A0")[0] + 1][0],
+            df[np.where(df=="A1")[0] + 1][0],
+            df[np.where(df=="A2")[0] + 1][0],
+            df[np.where(df=="A3")[0] + 1][0],
+            df[np.where(df=="A4")[0] + 1][0],
+            df[np.where(df=="A5")[0] + 1][0],
+            df[np.where(df=="A6")[0] + 1][0],
+            df[np.where(df=="A7")[0] + 1][0],
+            df[np.where(df=="A8")[0] + 1][0]
+        ]
+        print(self.coef)
+
+    def _Extended_3(self, lam):
+        a0 = self.coef[0]
+        a1 = self.coef[1]
+        a2 = self.coef[2]
+        a3 = self.coef[3]
+        a4 = self.coef[4]
+        a5 = self.coef[5]
+        a6 = self.coef[6]
+        a7 = self.coef[7]
+        a8 = self.coef[8]
+        lam /= 1000.0 # Convert to micrometers to use in the formula
+        n2 = a0 + a1 * lam**(2) + a2 * lam**(4) + a3 * lam**(-2) + a4 * lam**(-4) + a5 * lam**(-6) + a6 * lam**(-8) + a7 * lam**(-10) + a8 * lam**(-12)
+        return np.sqrt(n2)
 
     # TODO: add more decoder and formula here if needed 
 
 def main():
-    newglass = Material("LF7")
+    newglass = Material("J-SK2")
     newglass.DrawRI()
 
 if __name__ == "__main__":