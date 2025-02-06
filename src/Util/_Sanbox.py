b3a9c8a13424fefe56f6c2cce7cc3c4e694f3b5e
Chen <amarthgul@gmail.com>
2025-02-06 18:55:44 -0500

    Added a chart test iteration method

--- src/Util/_Sanbox.py
+++ src/Util/_Sanbox.py
@@ -1,81 +1,123 @@
 
 # For unused functions and tests 
 
-import numpy as np 
-
-from Globals import * 
-
-
-def WavelengthToRGB(wavelength, 
-                    primaries={"R": "C'", "G": "e", "B": "g"},
-                    UVIRcut=["i", "A'"],
-                    useBits=False, bitDepth=8):
-    """
-    Convert a wavelength to RGB values.
-    
-    :param wavelength: Wavelength to convert (in nm).
-    :param primaries: A dictionary mapping RGB to primary wavelength lines.
-    :param UVIRcut: List with UV and IR limits for cutoff.
-    :param bitDepth: Bit depth for RGB values.
-    :return: RGB values as integers in the range [0, 255].
-    """
-
-    # TODO: Note that this function works in conjunction with RGBToWavelength, \
-    # thus should be edited together with RGBToWavelength to ensure the results. 
-    # For this reason, there is no secondaries here since they are just linear interpolations and can be just ignored  
-
-    # Default RGB values (intensity normalized to 0-1)
-    R, G, B = 0.0, 0.0, 0.0
-
-    # Calculate bit depth scaling factor
-    bits = 2 ** bitDepth - 1
-
-    # Check where the wavelength falls and interpolate
-    if (LambdaLines[primaries["R"]] <= wavelength <= LambdaLines[UVIRcut[1]]):
-        # Between Red primary and IR cutoff
-        R  = (LambdaLines[UVIRcut[1]] - wavelength) / (LambdaLines[UVIRcut[1]] - LambdaLines[primaries["R"]])
-
-    elif (LambdaLines[primaries["G"]] <= wavelength < LambdaLines[primaries["R"]]):
-        # Between Green and Red
-        ratio = (wavelength - LambdaLines[primaries["G"]]) / (LambdaLines[primaries["R"]] - LambdaLines[primaries["G"]])
-        R = ratio
-        G = 1 - ratio
-
-    elif (LambdaLines[primaries["B"]] <= wavelength < LambdaLines[primaries["G"]]):
-        # Between Blue and Green
-        ratio = (wavelength - LambdaLines[primaries["B"]]) / (LambdaLines[primaries["G"]]- LambdaLines[primaries["B"]])
-        G = ratio
-        B = 1 - ratio
-
-    elif (LambdaLines[UVIRcut[0]] <= wavelength < LambdaLines[primaries["B"]]):
-        # Between Blue primary and UV cutoff
-        B = 1.0
-        G = (wavelength - LambdaLines[UVIRcut[0]]) / (LambdaLines[primaries["B"]] - LambdaLines[UVIRcut[0]])
-
-    # Scale to bit depth and return
-    
-    if (useBits):
-        R = int(np.clip(R * bits, 0, bits))
-        G = int(np.clip(G * bits, 0, bits))
-        B = int(np.clip(B * bits, 0, bits))
-
-    return np.array([R, G, B])
+import time
+import matplotlib.pyplot as plt
+from PIL import Image
 
 
-#  ===========================================================================
-"""
-==============================================================================
-"""
+from Util.Backend import backend as bd
+from Util.Misc import ImageConversion
+from Util.PltPlot import DrawRaybatch, AddXYZ, SetUnifScale, DrawPoints
+from ExampleLenses import Biotar50mmf14, Helios58mmf2, CanonFD50mmf18
+from Imagers.Standard import StdImager 
+from ObjectSpace.Points import PointsSource
+from ObjectSpace.Images import Image2D
+from Raytracing.Emission import EmitField
+
+
+
+def ISO12233Test(imageDistance = 200000, imageMinSample = 320):
+
+    lens = Biotar50mmf14()
+    #lens.SetAperture(22)
+
+    source = PointsSource()
+    source.isCartesian = False
+    source.GenerateSpots(19, 12)
+
+    imager = StdImager(lens.BestFocusBFD(imageDistance)) #32.4
+    # Assemble the imaging system 
+    imager.SetLensLength(lens.totalAxialLength)
+    image = imager.AccquireEmpty() 
+
+    sourceImage = Image2D()
+    sourceImage.horizontalAoV = 40
+    sourceImage.imageDimensionOverride = 1920 
+    sourceImage.distance = imageDistance
+    sourceImage.LoadFrom8bit(r"resources/ISO12233-4k.png") 
+    #sourceImage.SetupTransitionTest()
+    # Henri-Cartier-Bresson.png ISO12233-4k.png  Arrow.png Grid.png
+
+    start = time.time()
+
+    # plt.ion()  # Turn on interactive mode
+    # fig, ax = plt.subplots()
+    # im = ax.imshow(ImageConversion(image))
+    iterationCount = 0
+
+    while(True):
+        #print("- Starting a new sample iteration")
+        #mainRB = source.EmitSamplesToward(lens.entrancePupil.GetSamplePoints(40960), 5)
+        mainRB = sourceImage.EmitSamplesToward(lens.entrancePupil.GetSamplePoints(32), 409600)
+
+        lens.SetIncidentRaybatch(mainRB)
+
+        mainRB, mainRP = lens.Propagate()
+
+        mainRB, _tir, _vig = imager.IntersectRays(mainRB)
+        # mainRP.Append(mainRB, _tir, _vig)
+
+        image = imager.IntegralRays(mainRB, baseImg=image)
+
+        
+        # im.set_data(ImageConversion(image))
+        # plt.draw()
+        # plt.pause(0.01)
+        
+        #print(source.sampleRecord)
+        elpased = time.time() - start
+        imMin, imMax, imR = sourceImage.GetSampleRatios()
+
+        print(iterationCount, "th iteration finished a new sample iteration after ", elpased, "  \t Min: ", imMin, " max: ", imMax,  " -Ratio: ", imR)
+        iterationCount += 1
+        
+        if(imMin > imageMinSample):
+            imgSave = Image.fromarray(ImageConversion(image), 'RGB')
+            imgSave.save(r"resources/Results/Biotar_dist"+str(imageDistance)+"_" + str(imageMinSample) + "Sample.png")
+            break
+
+        return elpased 
+
+# =====================================================================
+""" =============================================================== """
+
 
 def main():
-    RGB = WavelengthToRGB(643.85) 
-    #wavelngth = RGBToWavelength(RGB)
-    RGB1 = WavelengthToRGB(589.3) 
 
-    print("RGB result: \t", RGB)
-    #print("wavelength: \t", wavelngth)
-    print("RGB result: \t", RGB1)
+    distList = {
+        400     :   20, 
+        450     :   20,
+        500     :   20, 
+        550     :   20, 
+        600     :   21, 
+        700     :   22, 
+        800     :   23, 
+        950     :   24, 
+        1200    :   25, 
+        1350    :   26 , 
+        1500    :   27, 
+        1750    :   28, 
+        2000    :   29, 
+        2500    :   30, 
+        3000    :   33, 
+        4000    :   36, 
+        5000    :   40, 
+        7500    :   50, 
+        10000   :   60, 
+        20000   :   70, 
+        50000   :   80, 
+        100000  :   90, 
+        200000  :   320
+    }
 
-#Rotation(0.61, np.array([0, 1, 0]), np.array([1, 1, 0]))
+    timeConsumed = []
+
+    for key, value in distList.items():
+        timeConsumed.append(ISO12233Test(key, value))
+
+    print(timeConsumed)
+
+
 if __name__ == "__main__":
     main() 
